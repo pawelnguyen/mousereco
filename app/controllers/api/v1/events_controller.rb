7ff1d4f0b0790796e26159e57d1877a20ce4fd81
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-05 14:23:57 +0200

    adjust request structure

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::EventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create
-    EventsService.create_collection(permitted_params[:events])
+    EventsService.create_collection(permitted_params[:events].values)
     render json: {success: true}
   end
 
--- spec/api/events_api_spec.rb
+++ spec/api/events_api_spec.rb
@@ -3,23 +3,12 @@ require 'spec_helper'
 describe 'POST /api/v1/events' do
   subject { post "/api/v1/events", data; response }
   let(:data) {
-    {
-      url: 'http://test.com',
-      user_key: '4k5n245j625k23nrg',
-      visitor_key: '49tuhiarf9q834tn34k3t',
-      events: [
-        {
-          x: '123.0',
-          y: '321.4',
-          timestamp: '123456671'
-        },
-        {
-          x: '121.0',
-          y: '320.4',
-          timestamp: '123456672'
-        }
-      ]
-    }
+    {"url"=>"http://test.com",
+     "user_key"=>"4k5n245j625k23nrg",
+     "visitor_key"=>"49tuhiarf9q834tn34k3t",
+     "events"=>
+       {"0"=>{"x"=>"123.0", "y"=>"321.4", "timestamp"=>"123456671"},
+        "1"=>{"x"=>"125.0", "y"=>"323.4", "timestamp"=>"123456678"}}}
   }
 
   its(:status) { should eq 200 }
