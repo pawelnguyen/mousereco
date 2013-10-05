0dae685b208b8549dcbbd44c9cc9d7d8d7fdeaf0
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-05 10:39:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MouseRecorder::Application.config.secret_key_base = '838c3316edd16833d9b293ee25b0c4fa7bbbc6d41a0b6dbe6afc47a2c6b737db279ff3d81ac7da6114bb99cd3033d8adfccb9a30cde37a514b028febb2421883'
