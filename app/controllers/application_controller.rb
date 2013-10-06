feef1b7e61ae0611b1a39cf66e7fd710e773d4c7
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-06 17:39:55 +0200

    fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def options
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-    headers["Access-Control-Allow-Headers"] =
-      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-    head(:ok)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ MouseRecorder::Application.routes.draw do
   namespace :api do
     namespace :v1 do
       post :events, to: 'events#create'
+      match '*all', to: 'application#options', via: [:options]
     end
   end
 
-  match '*all' => 'application#options', via: [:options]
   root 'home#index'
 end
