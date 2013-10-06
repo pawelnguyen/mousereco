a104ee19a9021d9eb047e0153cabb134a26c90a9
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-06 21:41:00 +0200

    show visitor pageviews

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe 'VisitorsController' do
+describe VisitorsController do
   describe '#index' do
-    subject { get "/visitors"; response }
+    subject { get :index; response }
 
     its(:status) { should eq 200 }
   end
