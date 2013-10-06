f9f5e0cd8a9a3605681353cfe61b805db847b292
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-06 12:33:20 +0200

    trying to fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def options
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
     head(:ok)
   end
 end
