491b1bb2c5fb3358efe8df9abe69a78cec1ae484
f1ames <dr.odpowiedz@gmail.com>
2013-10-21 22:01:37 +0200

    pageviews - wyswietlanie clickow

--- app/assets/javascripts/pageviews.js
+++ app/assets/javascripts/pageviews.js
@@ -4,17 +4,27 @@
                 var createIframe = function(url) {
                     return $('body').append('<iframe src="' + url + '" width="100%" height="600"></iframe>').find('iframe');
                 },
-                createPoint = function(x, y, i) {
-                    $('body').append('<div style="position:absolute;top:'+y+'px;left:'+x+'px;">'+i+'</div>');
+                createPoint = function(x, y, i, $el) {
+                    $el.append('<div style="position:absolute;top:'+y+'px;left:'+x+'px;">'+i+'</div>');
                 }
 
                 var $iframe = createIframe($('#events').attr('data-url')),
                     data = $.parseJSON($('#events').attr('data-events')),
                     offsetX = $iframe.offset().left,
                     offsetY = $iframe.offset().top;
-                $.each(data, function(i, o) {
-                    createPoint(o.x + offsetX, o.y + offsetY, i);
+
+                $('iframe').on('load', function() {
+                    var $iframeBody = $('iframe').contents().find('body')
+                    $.each(data, function(i, o) {
+                        createPoint(o.x, o.y, i, $iframeBody);
+                    });
                 });
+//                setTimeout(function(){
+//                    var $iframeBody = $('iframe').contents().find('body');
+//                    $.each(data, function(i, o) {
+//                        createPoint(o.x, o.y, i, $iframeBody);
+//                    });
+//                }, 2000);
             }
         });
 })();
