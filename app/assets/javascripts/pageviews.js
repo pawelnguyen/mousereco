b723ebe96eac3ef33c5f261b605bd5d5c0253b09
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-22 00:30:19 +0200

    set width and height of iframe window

--- app/assets/javascripts/pageviews.js
+++ app/assets/javascripts/pageviews.js
@@ -1,15 +1,19 @@
 (function() {
     $(document).on('ready', function() {
         if ($('body').hasClass('pageviews')) {
-            var createIframe = function(url) {
-                    return $('body').append('<iframe src="' + url + '" width="100%" height="600"></iframe>').find('iframe');
+            var createIframe = function(url, width, height) {
+                    var pageview_window = $('#pageview_window');
+                    pageview_window.attr('src', url);
+                    pageview_window.attr('width', width);
+                    pageview_window.attr('height', height);
+                    return pageview_window;
                 },
                 createPoint = function(x, y, type, i, $el) {
                     var color = type == 'click' ? 'blue' : 'pink';
                     $el.append('<div style="position:absolute;top:' + y + 'px;left:' + x + 'px;color:' + color + '">' + i + '</div>');
                 }
 
-            var $iframe = createIframe($('#events').attr('data-url')),
+            var $iframe = createIframe($('#events').attr('data-url'), $('#events').attr('data-window-width'), $('#events').attr('data-window-height')),
                 data = $.parseJSON($('#events').attr('data-events')),
                 offsetX = $iframe.offset().left,
                 offsetY = $iframe.offset().top;
