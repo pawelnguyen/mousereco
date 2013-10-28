73bfed047e9c200d136d0f94974e270424daa586
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-10-28 22:45:07 +0100

    fix tests

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe VisitorsController do
   describe '#index' do
+    let(:user) { User.create(email:'test@email.com', password: 'password', password_confirmation: 'password')}
+    before do
+      sign_in user
+    end
+
     subject { get :index; response }
 
     its(:status) { should eq 200 }
