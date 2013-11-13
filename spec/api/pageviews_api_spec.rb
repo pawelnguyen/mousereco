0d806f26e52a7bd9ec64229db86a3d2ea7fdad20
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-11-13 23:13:54 +0100

    save website key in pageviews api request

--- spec/api/pageviews_api_spec.rb
+++ spec/api/pageviews_api_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Api::V1::PageviewsController do
+  let!(:website) { Website.create(key: '4k5n245j625k23nrg')}
+
   describe '#preflight' do
     subject { post "/api/v1/pageviews/preflight", data; response }
     let(:data) {
