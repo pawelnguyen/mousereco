53513e75039be0d408cc2bf71525b7c5ebe0fb29
Paweł Nguyen <pawel.nguyen@gmail.com>
2013-11-24 12:49:48 +0100

    pageviews authorization

--- app/controllers/pageviews_controller.rb
+++ app/controllers/pageviews_controller.rb
@@ -1,5 +1,6 @@
 class PageviewsController < InheritedResources::Base
   before_filter :authenticate_user!
+  load_and_authorize_resource
   custom_actions resource: :page_html
 
   layout false, only: [:page_html]
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
     user ||= User.new
     if user.persisted?
       can :manage, Website, user_id: user.id
+      can :manage, Pageview do |pageview|
+        pageview.website.try(:user_id) == user.id
+      end
     end
   end
 end
